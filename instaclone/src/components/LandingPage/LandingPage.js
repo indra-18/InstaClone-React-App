2528503983c65d466d62c7002d9182b4c4772c95
indra-18 <indraind0918@gmail.com>
2023-03-29 19:59:49 +0530

    Edited Landing Page and added styles

--- instaclone/src/components/LandingPage/LandingPage.js
+++ instaclone/src/components/LandingPage/LandingPage.js
@@ -1,6 +1,19 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import lens from '../../images/lens.png'
+import './landing.css'
 
 export default function LandingPage() {
-  return <h2>Landing Page</h2>;
+  return <div id='landingPage-container'>
+    <div className='lens-container'>
+      <img src={lens} alt='lens' />
+    </div>
+    <div>
+      <h2 id='head' >10x Team 04</h2>
+      <div id='btn-container'>
+        <Link to='/view' >Enter</Link>
+      </div>
+
+    </div>
+  </div>;
 }
